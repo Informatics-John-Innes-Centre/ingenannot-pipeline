56649f0ac0e5d5e88379344adfe02e6585f2f949
William Redding <williamdredding@proton.me>
2026-08-12 14:36:08 +0100

    Use apptainer instead of singularity

--- containers.sh
+++ containers.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 # Pull biocontainers
-mkdir -p singularity-cache
+mkdir -p apptainer-cache
 
 # Build custom containers
-sudo singularity build singularity-cache/bedtools.sif containers/bedtools.def
-sudo singularity build singularity-cache/helixer.sif containers/helixer.def
-sudo singularity build singularity-cache/annevo.sif containers/annevo.def
-sudo singularity build singularity-cache/tiberius-1.1.8.sif containers/tiberius-1.1.8.def
-sudo singularity build singularity-cache/tiberius-2.0.6.sif containers/tiberius-2.0.6.def
-sudo singularity build singularity-cache/miniprot.sif containers/miniprot.def
-sudo singularity build singularity-cache/cutadapt.sif containers/cutadapt.def
-sudo singularity build singularity-cache/star.sif containers/star.def
-sudo singularity build singularity-cache/braker3.sif containers/braker3.def
-sudo singularity build singularity-cache/stringtie.sif containers/stringtie.def
-sudo singularity build singularity-cache/ingenannot.sif containers/ingenannot.def
-sudo singularity build singularity-cache/samtools.sif containers/samtools.def
+pixi run apptainer build apptainer-cache/bedtools.sif containers/bedtools.def
+pixi run apptainer build apptainer-cache/helixer.sif containers/helixer.def
+pixi run apptainer build apptainer-cache/annevo.sif containers/annevo.def
+pixi run apptainer build apptainer-cache/tiberius-1.1.8.sif containers/tiberius-1.1.8.def
+pixi run apptainer build apptainer-cache/tiberius-2.0.6.sif containers/tiberius-2.0.6.def
+pixi run apptainer build apptainer-cache/miniprot.sif containers/miniprot.def
+pixi run apptainer build apptainer-cache/cutadapt.sif containers/cutadapt.def
+pixi run apptainer build apptainer-cache/star.sif containers/star.def
+pixi run apptainer build apptainer-cache/braker3.sif containers/braker3.def
+pixi run apptainer build apptainer-cache/stringtie.sif containers/stringtie.def
+pixi run apptainer build apptainer-cache/ingenannot.sif containers/ingenannot.def
+pixi run apptainer build apptainer-cache/samtools.sif containers/samtools.def
 
